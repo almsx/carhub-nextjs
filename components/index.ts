e9e39c9594ae6504eb4617a4942735efde36e3cc
Alberto Luebbert M <alms@MacPro15.local>
2023-07-10 18:25:33 -0600

    :sparkles: WebSite with NextJS init

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+import SearchBar from "./SearchBar";
+import CarDetails from "./CarDetails";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+
+export {
+  Hero,
+  Footer,
+  NavBar,
+  CarCard,
+  ShowMore,
+  SearchBar,
+  CarDetails,
+  CustomButton,
+  CustomFilter,
+  SearchManufacturer,
+};
